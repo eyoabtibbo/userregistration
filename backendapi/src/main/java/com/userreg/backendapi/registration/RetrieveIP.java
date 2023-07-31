6b8f8d0bfbab63b86367439e437fce69d81c7415
eyoabtibbo <eyoab.m.tibbo@gmail.com>
2023-07-31 15:55:45 -0300

    added a class to retrieve iP Address

---
+++ backendapi/src/main/java/com/userreg/backendapi/registration/RetrieveIP.java
@@ -0,0 +1,25 @@
+package com.userreg.backendapi.registration;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class RetrieveIP {
+
+    private static final String [] headers = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP"
+    };
+
+    public static String getUserIP(HttpServletRequest request) {
+
+        for (String header: headers) {
+            String addresses = request.getHeader(header);
+            if(addresses.isEmpty() || addresses == null) {
+                continue;
+            }
+            String [] address = addresses.split("\\s*, \\s*");
+            return address[0];
+        }
+        return request.getRemoteAddr();
+    }
+}
